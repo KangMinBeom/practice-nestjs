735f5d37dd6b4d3aa66fc348c0257c087df9366a
kangminbeom <alsqja6945@naver.com>
2024-03-11 15:43:28 +0900

    payment.module 추가

---
+++ src/payment/payment.module.ts
@@ -0,0 +1,49 @@
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Coupon } from './entity/coupon.entity';
+import { IssuedCoupon } from './entity/issued-coupon.entity';
+import { Order } from './entity/order.entity';
+import { OrderItem } from './entity/order-item.entity';
+import { Point } from './entity/point.entity';
+import { PointLog } from './entity/point-log.entity';
+import { Product } from './entity/product.entity';
+import { ShippingInfo } from './entity/shipping-info.entity';
+import { AuthModule } from '../auth/auth.module';
+import { PaymentService } from './service/payment.service';
+import { ProductService } from './service/product.service';
+import { CouponRepository } from './Repository/coupon.repository';
+import { IssuedCouponRepository } from './Repository/issued-coupon.repository';
+import { OrderItemRepository } from './Repository/order-item.repository';
+import { OrderRepository } from './Repository/order.repository';
+import { PointLogRepository } from './Repository/point-log.repository';
+import { PointRepository } from './Repository/point.repository';
+import { ProductRepository } from './Repository/product.repository';
+import { ShippingInfoRepository } from './Repository/shipping-info.repository';
+@Module({
+  imports: [
+    AuthModule,
+    TypeOrmModule.forFeature([
+      Order,
+      OrderItem,
+      ShippingInfo,
+      Point,
+      PointLog,
+      Coupon,
+      IssuedCoupon,
+      Product,
+    ]),
+  ],
+  providers: [
+    PaymentService,
+    ProductService,
+    OrderRepository,
+    OrderItemRepository,
+    ShippingInfoRepository,
+    ProductRepository,
+    CouponRepository,
+    IssuedCouponRepository,
+    PointRepository,
+    PointLogRepository,
+  ],
+})
+export class PaymentModule {}
